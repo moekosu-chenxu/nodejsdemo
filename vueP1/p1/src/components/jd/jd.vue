8c60beeee2b631928c41efce7d141eaf99db802f
moekosu-chenxu <30981903+moekosu-chenxu@users.noreply.github.com>
2018-09-10 01:27:32 +0800

    jd index

--- vueP1/p1/src/components/jd/jd.vue
+++ vueP1/p1/src/components/jd/jd.vue
@@ -22,8 +22,89 @@
           <div id="top_left_logo"></div>
           <div id="top_left_navi">
             <ul>
-                <li>电器</li>
-                <li>手机</li>
+              <li><span class="left_navi_item">家用电器</span></li>
+                <li>
+                  <span class="left_navi_item">手机</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">运营商</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">数码</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">电脑</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">办公</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">家居</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">家具</span><i class="navi_oper">/</i>
+                    <span class="left_navi_item">家装</span><i class="navi_oper">/</i>
+                      <span class="left_navi_item">厨具</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">男装</span><i class="navi_oper">/</i>
+                    <span class="left_navi_item">女装</span><i class="navi_oper">/</i>
+                      <span class="left_navi_item">童装</span><i class="navi_oper">/</i>
+                        <span class="left_navi_item">内衣</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">美妆</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">个护清洁</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">宠物</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">女鞋</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">箱包</span><i class="navi_oper">/</i>
+                    <span class="left_navi_item">钟表</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">珠宝</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">男鞋</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">运动</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">户外</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">房产</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">汽车</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">汽车用品</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">母婴</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">玩具乐器</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">食品</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">酒类</span><i class="navi_oper">/</i>
+                    <span class="left_navi_item">生鲜</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">特产</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">艺术</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">礼品鲜花</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">医药保健</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">计生情趣</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">图书</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">音像</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">电子书</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">机票</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">酒店</span><i class="navi_oper">/</i>
+                    <span class="left_navi_item">旅游</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">生活</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">理财</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">众筹</span><i class="navi_oper">/</i>
+                    <span class="left_navi_item">白条</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">保险</span>
+                </li>
+                <li>
+                  <span class="left_navi_item">安装</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">维修</span><i class="navi_oper">/</i>
+                  <span class="left_navi_item">清洗保养</span>
+                </li>
               </ul>
           </div>
         </div>
@@ -31,21 +112,38 @@
           <div id="top_right_search">
             <div id="search_div">
               <input id="search_text" type="text" placeholder="查询商品" />
-              <div id="search_btn"></div>
-              <div id="search_ad">
-                <ul>
-                  <li>开学季特惠</li>
-                  <li>海鲜水产</li>
-                  <li>游戏手机</li>
-                </ul>
-              </div>
+              <div id="search_btn"><i>ρ</i></div>
+              <div id="buyCar">我的购物车</div>
             </div>
-            <div id="buyCar"></div>
+            <div id="search_ad">
+              <ul>
+                <li>开学季特惠</li>
+                <li>海鲜水产</li>
+                <li>游戏手机</li>
+              </ul>
+            </div>
           </div>
-          <div id="top_right_classify"></div>
+          <div id="top_right_classify">
+            <ul>
+              <li>秒杀</li>
+              <li>优惠券</li>
+              <li>PLUS会员</li>
+              <li>闪购</li>
+              <li>拍卖</li>
+              <li>京东服饰</li>
+              <li>京东超市</li>
+              <li>生鲜</li>
+              <li>全球购</li>
+              <li>京东金融</li>
+            </ul>
+          </div>
           <div id="top_right_ad">
             <div id="top_right_ad_bigAd"></div>
-            <div id="top_right_ad_fixedAd"></div>
+            <div id="top_right_ad_fixedAd">
+              <div class="top_right_ad_fixedAd1"></div>
+              <div class="top_right_ad_fixedAd1"></div>
+              <div class="top_right_ad_fixedAd2"></div>
+            </div>
             <div id="top_right_ad_hello"></div>
           </div>
         </div>
@@ -86,13 +184,23 @@ export default {
   li{
     list-style: none;
   }
+  .navi_oper{
+    padding: 0 2px;
+    font-size: 12px;
+  }
   /*页面框架*/
+  #jd{
+    width: 100%;height: 100%;
+    background-color: #f0f3ef;
+  }
   #box {
     width: 1190px;height:auto;
     min-width: 1190px;
     margin: 0 auto;
   }
   #top {
+    width: 100%;height: 652px;
+    margin-bottom: 60px;
   }
   #top_left{
     width: 190px;float: left;
@@ -102,9 +210,14 @@ export default {
     background: url("../../images/jd_head.png");
     border: 1px solid #ccc;
     box-shadow: 0 -12px 10px rgba(0,0,0,.2);
+    float: left;
   }
   #top_left_navi{
     width: 190px;height: 480px;
+    border-left: 1px solid  #ccc;
+    border-right: 1px solid  #ccc;
+    float: left;
+    font-size: 13px;
   }
   #top_left_navi li{
     height: 27px;line-height: 27px;
@@ -112,7 +225,7 @@ export default {
     text-align: left;
   }
   #top_right{
-    width: 77%;float: right;
+    width: calc(100% - 195px);float: right;
   }
   #top_right_navi{
     width: 100%;height: 30px;
@@ -144,21 +257,94 @@ export default {
     color: #737373;
   }
   #top_right_search{
-    height: 80px;width: 100%;
+    height: 130px;width: 100%;
+    text-align: left;
+    float: left;
+  }
+  #search_div{
+    height: 35px;width: 100%;
+    padding-top: 45px;padding-left: 120px;
+    float: left;
+  }
+  #search_div #search_text{
+    width: 450px;height: 25px;line-height: 25px;float: left;
+    padding: 4px 44px 4px 4px;
+    font-size: 14px;
+    text-indent: 5px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+  }
+  #search_div #search_btn{
+    height: 35px;width: 50px;
+    text-align: center;line-height: 35px;
+    float: left;
+    background-color: #f10215;
+  }
+  #search_div #buyCar{
+    height: 30px;width: 150px;
+    text-align: center;line-height: 30px;
+    float: left;
+    margin-top: 2px;margin-left: 40px;
+    background-color: #fff;border: 1px solid #ccc;
+    box-shadow: 0 0 5px #ccc;
+    font-size: 12px;color: #f10214;
+  }
+  #search_ad{
+    height: 20px;width: 100%;
+    float: left;
+    padding-left: 120px;
+  }
+  #search_ad ul{
+    margin-top: 5px;
+    font-size: 13px;
+  }
+  #search_ad li{
+    float: left;
+    margin-right: 10px;
   }
   #top_right_classify{
     width: 100%;height: 40px;
+    line-height: 60px;
+    float: left;
+  }
+  #top_right_classify ul{
+    margin: 0;
+  }
+  #top_right_classify li{
+    font-size: 14px;text-align: center;
+    float: left;
+    margin-left: 40px;
   }
   #top_right_ad{
     width: 100%;height: 480px;
+    float: left;
+    padding: 10px 0 0 10px;box-sizing: border-box;
   }
   #top_right_ad_bigAd{
-    height: 100%;width: 50%;
+    height: 100%;width: 570px;
+    margin-right: 10px;
+    float: left;
+    border: 1px solid #ccc;
   }
   #top_right_ad_fixedAd{
-    height: 100%;width: 20%;
+    height: 100%;width: 190px;
+    margin-right: 10px;
+    float: left;
+  }
+  #top_right_ad_fixedAd .top_right_ad_fixedAd1{
+    height: 150px;width: 100%;
+    margin-bottom: 8px;
+    float: left;
+    border: 1px solid #ccc;
+  }
+  #top_right_ad_fixedAd .top_right_ad_fixedAd2{
+    height: 150px;width: 100%;
+    float: left;
+    border: 1px solid #ccc;
   }
   #top_right_ad_hello{
     height: 100%;width: 20%;
+    float: left;
+    border: 1px solid #ccc;
   }
 </style>
